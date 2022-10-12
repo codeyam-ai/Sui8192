985531200548a98b9b3a1a28f82308b60ae89bb7
Jared Cosulich <jared.cosulich@gmail.com>
2022-10-12 18:33:22 -0400

    new contract address

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  contractAddress: "0x67106a09142eb686f8cc4f400d781d483254793",
-  leaderboardAddress: "0x1838d8fe1a433d4b8bfd3f56594cd180b3f527b5",
+  contractAddress: "0xe611fa468ca9e6e5bd9f37dfa663b231b07f4023",
+  leaderboardAddress: "0x4225633a86d8b855402e84501a193c6163744303",
   tileNames: {
     1: "Air",
     2: "Mist",
