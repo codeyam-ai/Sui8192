c4e682f952e62f09c153513348143b3e25facb64
Jared Cosulich <jared.cosulich@gmail.com>
2022-12-27 12:10:50 -0500

    new contract

--- build/index.js
+++ build/index.js
@@ -160,8 +160,8 @@ module.exports = {
 };
 },{"canvas-confetti":44}],3:[function(require,module,exports){
 module.exports = {
-  contractAddress: "0x38429db0b4ca67f3e1cbacae75db7eb63b4972d1",
-  leaderboardAddress: "0x26cc642a6364dba72162343564d2796d191f574c",
+  contractAddress: "0xd5572d348836d27c5e93fa8a27c481f66a7a5a83",
+  leaderboardAddress: "0xee16c19ea94c11e9627c429278dffb15e227c721",
   tileNames: {
     1: "Air",
     2: "Mist",
