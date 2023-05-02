d0115c62548e3686c4bdabbc1f1ec28ebf847478
Jared Cosulich <jared.cosulich@gmail.com>
2023-05-02 07:25:09 -0400

    new contracts

--- build/index.js
+++ build/index.js
@@ -174,12 +174,12 @@ module.exports = {
 };
 },{"canvas-confetti":61}],3:[function(require,module,exports){
 module.exports = {
-  testnetContractAddress: "0x32ccb952525a8b7319234a80febff3dc02dae0f95891dcd7f9f4e702874be9fe",
-  testnetLeaderboardAddress: "0xd441367566fa0c0f337a2f5ec6c6ddd90213806a809c08ad7691bcf78dcfc37d",
-  testnetMaintainerAddress: "0xf59b65b08b2cafa158b33ae90a4a8d0c1b729ba5e0526b4c6c8c0e6f02100584",
-  devnetContractAddress: "0x7c26976bdee5fc471f6ec8f48fdd89fec76cd4b9ca0bccce7d5181d89774baa5",
-  devnetLeaderboardAddress: "0x4faf77c565de249ec96050d9c46cc2c3ae71c34a8dd48463db7b852049cc5283",
-  devnetMaintainerAddress: "0xbb367e30649d58e6602120f034c0ae0f55c28f77fa4e2680eedd75dda7004f84",
+  testnetContractAddress: "0x4abf9d88b5898b5af5b37fd2945cad953fe819de5720eb432911938eaa5a1ae1",
+  testnetLeaderboardAddress: "0x4825603c24bb0394dfcd6b217bc88b6685387897598cb740043ee6ada14098af",
+  testnetMaintainerAddress: "0x28edc0bf3661264dfbeeb5458a94a179f904c3a0ecf5bf49d552fb280a3318b1",
+  devnetContractAddress: "0x9c2f505d50f081de8fb18664a4443b1f876abc53b37fe8745fb9188492734522",
+  devnetLeaderboardAddress: "0xbb87dba4e03cc16f74858220ce9d691d20316bdcb841d8d2d5b775e40b04a693",
+  devnetMaintainerAddress: "0x9c10594e6214e95f43ddaaab2c54b9226d4b8243e25bf68f73a76ffb3d640425",
   tileNames: {
     1: "Air",
     2: "Mist",
