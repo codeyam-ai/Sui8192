641d9a4bbee141c908898753673845d743252893
Jared Cosulich <jared.cosulich@gmail.com>
2023-05-11 15:55:49 -0400

    new testnet contract

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testnetContractAddress: "0xed9ebe0bba5ac978eeb6808c2d71a789dd97a760e43a33402761c27bd69a9284",
-  testnetLeaderboardAddress: "0x4ff7969fd02ac43d414a794476aa7c6701952a1b6b0ff8b8a17451e10af81f2d",
-  testnetMaintainerAddress: "0x1dff9c3642858921118c4f2d0581f17a8440b199aab7bf683e2556254151fc61",
+  testnetContractAddress: "0x990570514ac706f0f33d87db6d0f12d3a98be00373836851363dee5fd70119f2",
+  testnetLeaderboardAddress: "0xe7b3d2b862cf5935526d8d39c04d5f5803f70070704ba7ddaddd6cb4ab61ef28",
+  testnetMaintainerAddress: "0x86942ec75d27f7caeabb94178a560c2819cd73a284405243b0aab13a15feb9b6",
   mainnetContractAddress: "0x72f9c76421170b5a797432ba9e1b3b2e2b7cf6faa26eb955396c773af2479e1e",
   mainnetLeaderboardAddress: "0xa834ebce466a79a3e2136c05fadce0322318051e0609f208a5d42cc04e0a67a3",
   mainnetMaintainerAddress: "0x1d6d6770b9929e9d8233b31348f035a2a552d8427ae07d6413d1f88939f3807f",
