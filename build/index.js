d8168b310af9a2107a557ae2c4772d32e16ec880
Jared Cosulich <jared.cosulich@gmail.com>
2023-10-04 08:20:13 -0400

    fixing confetti

--- build/index.js
+++ build/index.js
@@ -150,15 +150,15 @@ module.exports = {
     // const colors = ['#bb0000', '#ffffff'];
 
     const frame = () => {
-      confetti({
-        particleCount: 2,
+      confetti.default({
+        particleCount: 200,
         angle: 60,
         spread: 55,
         origin: { x: 0 },
         // colors: colors
       });
-      confetti({
-        particleCount: 2,
+      confetti.default({
+        particleCount: 200,
         angle: 120,
         spread: 55,
         origin: { x: 1 },
